2d851b2be583c42853a81e997c57af70c941950e
Marcos Scorzoni <mlessa@gmail.com>
2018-11-05 10:25:00 +0000

    add header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
