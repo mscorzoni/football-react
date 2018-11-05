37ad589e9e9c62b3975de0102b197b46ab4c3674
Marcos Scorzoni <mlessa@gmail.com>
2018-11-05 21:14:44 +0000

    connect app with firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+  apiKey: "AIzaSyDth-9RqAQkYICIj3ZsxR_rgV-9fv1Z_lw",
+  authDomain: "m-city-407fc.firebaseapp.com",
+  databaseURL: "https://m-city-407fc.firebaseio.com",
+  projectId: "m-city-407fc",
+  storageBucket: "m-city-407fc.appspot.com",
+  messagingSenderId: "34918308054"
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+
+firebaseDB.ref('matches').once('value').then((snapshot) => {
+  console.log(snapshot.val())
+})
