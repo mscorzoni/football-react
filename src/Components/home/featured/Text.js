ea6e762c329ee98139f3a525985d57f9982d922a
Marcos Lessa <mlessa@gmail.com>
2018-11-05 14:43:35 +0000

    add animated fonts to homepage'

--- src/Components/home/featured/Text.js
+++ src/Components/home/featured/Text.js
@@ -32,10 +32,73 @@ class Text extends Component {
     </Animate>
   )
 
+  animatedFirst = () => (
+    <Animate
+         show={true}
+         start={{
+           opacity:0,
+           x: 503,
+           y: 450
+         }}
+         enter={{
+           opacity:[1],
+           x: [273],
+           y: [450],
+           timing:{duration: 500, ease: easePolyOut}
+         }}
+      >
+      {({opacity,x,y}) => {
+        return(
+          <div className="featured_first"
+            style={{
+                  opacity,
+                  transform: `translate(${x}px, ${y}px)`
+                    }}
+              >
+            League
+          </div>
+        )
+      }}
+    </Animate>
+  )
+
+
+  animatedSecond = () => (
+    <Animate
+         show={true}
+         start={{
+           opacity:0,
+           x: 503,
+           y: 586
+         }}
+         enter={{
+           opacity:[1],
+           x: [273],
+           y: [586],
+           timing:{delay: 400, duration: 500, ease: easePolyOut}
+         }}
+      >
+      {({opacity,x,y}) => {
+        return(
+          <div className="featured_second"
+            style={{
+                  opacity,
+                  transform: `translate(${x}px, ${y}px)`
+                    }}
+              >
+            Championships
+          </div>
+        )
+      }}
+    </Animate>
+  )
+
   render() {
     return (
       <div className="featured_text">
         {this.animatedNumber()}
+        {this.animatedFirst()}
+        {this.animatedSecond()}
       </div>
     );
   }
