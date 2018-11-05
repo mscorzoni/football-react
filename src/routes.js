983926afb0eb9b93c0ad548bb6deb2f5ce93fee4
Marcos Lessa <mlessa@gmail.com>
2018-11-05 14:12:57 +0000

    add animated stripes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './Components/home';
 
 const Routes = (props) => {
   return(
     <Layout>
-      Second Lines
+      <Switch>
+        <Route exact component={Home}/>
+      </Switch>
     </Layout>
   )
 }
