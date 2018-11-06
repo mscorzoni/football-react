84e0da1a3ae5a3949eac0ec5dc973ed88f61f595
Marcos Scorzoni <mlessa@gmail.com>
2018-11-06 10:09:54 +0000

    implemenent matches component

--- src/firebase.js
+++ src/firebase.js
@@ -14,5 +14,10 @@ const config = {
 firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
-const firebaseMatches = firebaseDB.ref('matches')
+const firebaseMatches = firebaseDB.ref('matches');
+
+export {
+  firebase,
+  firebaseMatches
+}
 
