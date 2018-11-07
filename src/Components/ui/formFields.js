8a49de9fbc988f17f75c96729f7d903bb8e30d96
Marcos Scorzoni <mlessa@gmail.com>
2018-11-07 17:44:24 +0000

    create edit matchs page

--- src/Components/ui/formFields.js
+++ src/Components/ui/formFields.js
@@ -18,6 +18,10 @@ const FormField = ({formdata, id, change}) => {
       case('input'):
         formTemplate = (
           <div>
+            { formdata.showlabel ? 
+              <div className="label_inputs">{formdata.config.label}</div>
+              : null
+            }
             <input
               {...formdata.config}
               value={formdata.value}
@@ -27,6 +31,31 @@ const FormField = ({formdata, id, change}) => {
           </div>
         )
       break;
+      case('select'):
+        formTemplate = (
+          <div>
+            { formdata.showlabel ? 
+              <div className="label_inputs">{formdata.config.label}</div>
+              : null
+            }
+            <select>
+              value={formdata.value}
+              onChange={(event) => change({event, id})}
+            />
+              <option value="">Select One</option>
+              {
+                formdata.config.options.map((item) => (
+                  <option key={item.key} value={item.key}>
+                    {item.value}
+                  </option>
+                ))
+              }
+            </select>
+            {showError()}
+          </div>
+        )
+      break;
+
       default:
         formTemplate = null;
     }
