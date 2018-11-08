f8972c9e95bc997d8dbed09b27a0e3cefdaccfa1
Marcos Scorzoni <mlessa@gmail.com>
2018-11-08 17:22:32 +0000

    implement the team page

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
-import { Switch }  from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import PrivateRoute from './Components/authRoutes/privateRoutes';
 import PublicRoute from './Components/authRoutes/publicRoutes';
 
 import Home from './Components/home';
 import SignIn from './Components/signin';
+import TheTeam from './Components/theTeam';
 
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
@@ -15,19 +16,20 @@ import AdminPlayers from './Components/admin/players';
 import AddEditPlayers from './Components/admin/players/addEditPlayers';
 
 const Routes = (props) => {
-  return(
+  return (
     <Layout>
-        <Switch>
-            <PrivateRoute {...props} path="/admin_players/add_players" exact component={AddEditPlayers}/>
-            <PrivateRoute {...props} path="/admin_players/add_players/:id" exact component={AddEditPlayers}/>
-            <PrivateRoute {...props} path="/admin_players" exact component={AdminPlayers}/>
-            <PrivateRoute {...props} path="/admin_matches/edit_match" exact component={AddEditMatch}/>
-            <PrivateRoute {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch}/>
-            <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
-            <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
-            <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
-            <PublicRoute {...props} restricted={false} path="/" exact component={Home}/>
-        </Switch>
+      <Switch>
+        <PrivateRoute {...props} path="/admin_players/add_players" exact component={AddEditPlayers} />
+        <PrivateRoute {...props} path="/admin_players/add_players/:id" exact component={AddEditPlayers} />
+        <PrivateRoute {...props} path="/admin_players" exact component={AdminPlayers} />
+        <PrivateRoute {...props} path="/admin_matches/edit_match" exact component={AddEditMatch} />
+        <PrivateRoute {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch} />
+        <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches} />
+        <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
+        <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn} />
+        <PublicRoute {...props} restricted={false} path="/the_team" exact component={TheTeam} />
+        <PublicRoute {...props} restricted={false} path="/" exact component={Home} />
+      </Switch>
     </Layout>
   )
 }
